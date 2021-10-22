14bd4d2edc92d6360aea086cf05d26ce08a0d000
GitHub Action <action@github.com>
2021-10-22 06:35:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/coldtea97/emmluatest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/coldtea97/emmluatest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.coldtea97.emmluatest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.coldtea97.emmluatest.MyBundle
 
 class MyApplicationService {
 
